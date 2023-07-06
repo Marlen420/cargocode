388f41822eda2e80f433886fe10c6c3dcd6df8ed
Тимур Пратов <timur.pratovv@gmail.com>
2023-07-06 16:58:46 +0600

    added aws s3 mo and services

--- src/aws-s3/aws-s3.module.ts
+++ src/aws-s3/aws-s3.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AwsS3Service } from './aws-s3.service';
 
 @Module({
-  providers: [AwsS3Service]
+  providers: [AwsS3Service],
+  exports:[AwsS3Service],
 })
 export class AwsS3Module {}
